e307a4e9744b14045d14e21e3ddb575daaf9e274
keirrr <hadekshd@gmail.com>
2022-08-28 23:48:59 +0200

    Add basic client side validation

--- src/components/menus/MenuRegister.js
+++ src/components/menus/MenuRegister.js
@@ -4,8 +4,10 @@ import { Link } from "react-router-dom";
 // React
 import { useState } from "react";
 
+import validator from 'validator';
+
 // Axios
-import createUser from '../../features/createUser'
+import createUser from "../../features/createUser";
 
 // Components
 import TextInput from "../inputs/TextInput";
@@ -20,6 +22,11 @@ const MenuRegister = () => {
     repassword: "",
   });
 
+  const [isNameValid, setIsNameValid] = useState(true)
+  const [isEmailValid, setIsEmailValid] = useState(true)
+  const [isPasswordValid, setIsPasswordValid] = useState(true)
+  const [isRepasswordValid, setIsRepasswordValid] = useState(true)
+
   const valueHandler = (elem, value) => {
     setUserData((prevState) => ({
       ...prevState,
@@ -27,8 +34,61 @@ const MenuRegister = () => {
     }));
   };
 
-  const submitHandler = () => {
-    console.log(userData);
+  // Validation (but not DRY yet xD)
+  const checkNameInput = () => {
+    if (userData.name.trim() === ""){
+      setIsNameValid(false)
+    } else {
+      setIsNameValid(true)
+    }
+  }
+
+  const checkEmailInput = () => {
+    if (userData.email.trim() === ""){
+      setIsEmailValid(false)
+      return
+    } else {
+      if (validator.isEmail(userData.email) === false){
+        setIsEmailValid(false)
+        return
+      }
+      setIsEmailValid(true)
+    }
+  }
+
+  const checkPasswordInput = () => {
+    if (userData.password.trim() === ""){
+      setIsPasswordValid(false)
+    } else {
+      if(userData.repassword !== userData.password){
+        setIsPasswordValid(false)
+        setIsRepasswordValid(false)
+        return
+      }
+      setIsPasswordValid(true)
+    }
+  }
+
+  const checkRepasswordInput = () => {
+    if (userData.repassword.trim() === ""){
+      setIsRepasswordValid(false)
+    } else {
+      if(userData.repassword !== userData.password){
+        setIsPasswordValid(false)
+        setIsRepasswordValid(false)
+        return
+      }
+      setIsRepasswordValid(true)
+    }
+  }
+
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    
+
+    createUser(userData.name, userData.email, userData.password);
   };
 
   return (
@@ -48,35 +108,49 @@ const MenuRegister = () => {
         </span>
         <div className="p-1 w-[24px] h-[24px]"></div>
       </div>
-      <div className="flex flex-col justify-center">
+      <form onSubmit={submitHandler} className="flex flex-col justify-center">
         <TextInput
           placeholder="Imie"
           inputType="text"
           updateState={valueHandler}
           elemToUpdate="name"
+          min="2"
+          max="30"
+          pattern="[a-zA-Z]{2,30}"
+          checkInput={checkNameInput}
+          isValid={isNameValid}
         />
         <TextInput
           placeholder="Adres e-mail"
           inputType="text"
           updateState={valueHandler}
           elemToUpdate="email"
+          checkInput={checkEmailInput}
+          isValid={isEmailValid}
         />
         <TextInput
           placeholder="Hasło"
           inputType="password"
           updateState={valueHandler}
           elemToUpdate="password"
+          min="8"
+          max="64"
+          pattern="/(?=(.*[0-9]))((?=.*[A-Za-z0-9])(?=.*[A-Z])(?=.*[a-z]))^.{8,64}$/"
+          checkInput={checkPasswordInput}
+          isValid={isPasswordValid}
         />
         <TextInput
           placeholder="Powtórz hasło"
           inputType="password"
           updateState={valueHandler}
           elemToUpdate="repassword"
+          min="8"
+          max="64"
+          pattern="/(?=(.*[0-9]))((?=.*[A-Za-z0-9])(?=.*[A-Z])(?=.*[a-z]))^.{8,64}$/"
+          checkInput={checkRepasswordInput}
+          isValid={isRepasswordValid}
         />
-        <button
-          onClick={createUser}
-          className="h-[40px] w-full mt-[20px] bg-yellow-400 rounded-[10px] transition-colors hover:bg-yellow-300 active:bg-yellow-500"
-        >
+        <button className="h-[40px] w-full mt-[20px] bg-yellow-400 rounded-[10px] transition-colors hover:bg-yellow-300 active:bg-yellow-500">
           <span className="font-bold">Zarejestruj się</span>
         </button>
         <div className="flex justify-center mt-[10px]">
@@ -88,7 +162,7 @@ const MenuRegister = () => {
             <span className="underline">Zaloguj się</span>
           </Link>
         </div>
-      </div>
+      </form>
     </section>
   );
 };
