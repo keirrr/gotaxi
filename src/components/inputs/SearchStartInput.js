104503441f3f9fd222e8ec4f4b3f14a1ce53b4b0
keirrr <hadekshd@gmail.com>
2022-10-09 22:43:28 +0200

    Get current location of user

--- src/components/inputs/SearchStartInput.js
+++ src/components/inputs/SearchStartInput.js
@@ -1,9 +1,38 @@
 import React, { useState } from "react";
 
+import axios from "axios";
+
 import { BiCurrentLocation } from "react-icons/bi";
 
 const SearchStartInput = () => {
-  const [isFocused, setIsFocused] = useState(false)
+  const [isFocused, setIsFocused] = useState(false);
+
+  const [userLocation, setUserLocation] = useState([]);
+  const [locationName, setLocationName] = useState("");
+
+  const getCurrentPosition = async (props) => {
+    const success = (position) => {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+
+      const res = axios.get(
+        `https://nominatim.openstreetmap.org/search?q=${latitude},${longitude}&polygon_geojson=1&format=json`
+      );
+
+      res.then((res) => {
+        const locationInfo = res.data[0].display_name.split(",");
+        setLocationName(locationInfo[0].trim() + " " + locationInfo[1].trim());
+      });
+    };
+
+    const error = () => {
+      console.log("Something went wrong!");
+    };
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, error);
+    }
+  };
 
   return (
     <div className="flex justify-between relative h-10 w-full mt-[10px] bg-gray-200 rounded-[10px]">
@@ -18,8 +47,15 @@ const SearchStartInput = () => {
         className="absolute w-full h-full t-0 px-[40px] py-[5px] bg-gray-200 rounded-[10px] focus:outline-none focus:ring-2 focus:ring-gray-500 placeholder:text-gray-600"
         onFocus={() => setIsFocused(true)}
         onBlur={() => setIsFocused(false)}
+        onChange={() => {}}
+        value={locationName}
       />
-      <button className={`flex items-center mx-[10px] z-10 transition ${isFocused ? 'opacity-100' : 'opacity-0'}`}>
+      <button
+        onClick={getCurrentPosition}
+        className={`flex items-center mx-[10px] z-10 transition ${
+          isFocused ? "opacity-100" : "opacity-0"
+        }`}
+      >
         <BiCurrentLocation color="#111827" className="w-[20px] h-[20px]" />
       </button>
     </div>
